43c61f03a0567fcd310d8ffa4b479b3648f20b44
Samanvith <Samanvith2005@gmail.com>
2023-10-10 20:22:56 +0530

    Added Home component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Components/Header/Header';
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -6,14 +6,27 @@ import Quiz from './Pages/Quiz/Quiz';
 import Result from './Pages/Result/Result';
 import Home from './Pages/Home/Home';
 import Footer from './Components/Footer/Footer';
+import axios from "axios"
 
 const App = () => {
+  const[name,setname]=useState('')
+   const[Questions,setQuestions]=useState('')
+   const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${
+        category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    );
+
+    setQuestions(data.results);
+  };
   return (
     <BrowserRouter>
       <div className="app" style={{ backgroundImage: `url("/quiz.png")` }}>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home name={name}  setname={setname} fetchQuestions={fetchQuestions}/>} />
+
           <Route path="/Quiz" element={<Quiz />} />
           <Route path="/Result" element={<Result />} />
         </Routes>
--- my-app/src/Pages/Home/Home.js
+++ my-app/src/Pages/Home/Home.js
@@ -1,11 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Home.css';
+import { Button, MenuItem, TextField } from '@mui/material';
+import Categories from '../../Data/Category/Category';
+import {  useNavigate } from 'react-router-dom';
+import ErrorMessage from '../../Components/Message/ErrorMeesage';
 
-const Home = () => {
+
+const Home = ({ name, setname, fetchQuestions }) => {
+  const [category, setcategory] = useState('');
+  const [Difficulty, setDifficulty] = useState('0');
+  const [error, seterror] = useState(false);
+  const history = useNavigate();
+
+  const handleSubmit = () => {
+    if (!name || !category || !Difficulty) {
+      seterror(true);
+      return;
+    } else {
+      seterror(false);
+      fetchQuestions(category, Difficulty);
+      history('/Quiz');
+    }
+  };
+
   return (
-    <div>
-       Home page
+    <div className='Content'>
+      <div className='settings'>
+        <span style={{ fontSize: 30 }}>Quiz Settings</span>
+        <div className='Settings_select'>
+          {/* Add content here or remove this div if not needed */}
+          {error && <ErrorMessage>Please Fill all the Details</ErrorMessage>}
+          <TextField
+            label="Enter Your name"
+            variant="outlined"
+            value={name}
+            onChange={(e) => setname(e.target.value)}
+            style={{ marginBottom: "30px" }}
+          />
+          <TextField
+            select
+            label="Select Category"
+            variant="outlined"
+            value={category}
+            onChange={(e) => setcategory(e.target.value)}
+            style={{ marginBottom: "30px" }}
+          >
+            {Categories.map((cat) => (
+              <MenuItem key={cat.category} value={cat.value}>
+                {cat.category}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            select
+            label="Select Difficulty"
+            variant='outlined'
+            value={Difficulty}
+            onChange={(e) => setDifficulty(e.target.value)}
+            style={{ marginBottom: "30px" }}
+          >
+            <MenuItem key="easy" value="easy">easy</MenuItem>
+            <MenuItem key="medium" value="medium">medium</MenuItem>
+            <MenuItem key="Hard" value="Hard">Hard</MenuItem>
+          </TextField>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={handleSubmit}
+          >
+            Start Quiz
+          </Button>
+        </div>
+      </div>
+      <img src="/quiz.svg" className="banner" alt="quiz.svg" />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
